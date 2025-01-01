6a94114b67edd065d48b1af4f4e521ec3384667f
Dario Puddinu <dario.puddinu@abinsula.com>
2025-01-01 22:30:46 +0100

    feat: add favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(), sitemap()],
+  integrations: [
+    tailwind(),
+    compress(),
+    sitemap()
+  ],
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport'
