cddcd93aafaea0fae38ced60efe4b36138c2ec73
Dario Puddinu <dario.puddinu@abinsula.com>
2025-01-05 22:53:02 +0100

    refactor: slug pages

--- src/utils/isCurrentPage.ts
+++ src/utils/isCurrentPage.ts
@@ -1,6 +1,3 @@
 export function isCurrentPage(url: string, href: string) {
-  if (href === url) {
-    return true;
-  }
-  return url.split('/').filter(Boolean).includes(href.split('/')[1]);
+  return url === href;
 }
